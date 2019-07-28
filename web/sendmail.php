c47f6cf79f0a2b15fc83c7fc0416d0dd11cf8013
Jonathan <la_morgue4@hotmail.com>
2019-07-28 18:25:45 -0500

    Plantilla de correo

--- web/sendmail.php
+++ web/sendmail.php
@@ -4,14 +4,53 @@ $destinatario = "jvaldl00@gmail.com" . ", ";
 $destinatario .= "claumn87@gmail.com" . ", ";
 $destinatario .= "administracion@acindustrialsolutions.com"; 
 
-$asunto = "CONTACTO WEB AC INDUSTRIAL SOLUTIONS"; 
-
 $nombre = strip_tags($_POST['name']);
 $correo = strip_tags($_POST['email']);
-$telefono = strip_tags($_POST['subject']);
+$asunto = 'WEB:' . strip_tags($_POST['subject']);
 $mensaje = strip_tags($_POST['message']);
 
-$cuerpo = '<p>Nombre:'. $nombre . '</p>' . '<p>Correo:' . $correo .'</p>' . '<p>Tel:' . $asunto . '</p>' . '<p>Mensaje:' . $mensaje . '</p>'; 
+$cuerpo =  '<table width="50%" align="center" border="0" cellspacing="0" cellpadding="0">';
+$cuerpo .= '    <tr>';
+$cuerpo .= '        <td align="center" style="padding: 0 0 0 3px; background:#249285; color:white; font-size:larger">';
+$cuerpo .= '            <div style="display: table; height: 100px; overflow: hidden;">';
+$cuerpo .= '                <div style="display: table-cell; vertical-align: middle;">';
+$cuerpo .= '                    <img src="http://acindustrialsolutions.com/images/logo.png" alt="">';
+$cuerpo .= '                    <br />';
+$cuerpo .= '                    <div>';
+$cuerpo .= '                        MENSAJE DESDE EL PORTAL WEB';
+$cuerpo .= '                    </div>';
+$cuerpo .= '                </div>';
+$cuerpo .= '            </div>';
+$cuerpo .= '        </td>';
+$cuerpo .= '    </tr>';
+$cuerpo .= '    <tr>';
+$cuerpo .= '        <td class="h1" style="padding: 5px 0 0 0; text-wrap:normal; text-align:justify">';
+$cuerpo .= '            <br />';
+$cuerpo .= '				<p>Nombre:' . $nombre ;
+$cuerpo .= '				</p>' ;
+$cuerpo .= '				<p>Correo:' . $correo;
+$cuerpo .= '				</p>';
+$cuerpo .= '				<p>Mensaje:' . $mensaje;
+$cuerpo .= '				</p>';
+$cuerpo .= '        </td>';
+$cuerpo .= '    </tr>';
+$cuerpo .= '    <tr>';
+$cuerpo .= '        <td >';
+$cuerpo .= '            <br />';
+$cuerpo .= '        </td>';
+$cuerpo .= '    </tr>';
+$cuerpo .= '    <tr align="center" style="padding: 0 0 0 3px; background:#249285; color:white; font-size:small">';
+$cuerpo .= '        <td>';
+$cuerpo .= '            <div style="display: table; height: 20px; overflow: hidden;">';
+$cuerpo .= '                <div style="display: table-cell; vertical-align: middle;">';
+$cuerpo .= '                    <div>';
+$cuerpo .= '                       AC. Industrial Solutions.';
+$cuerpo .= '                    </div>';
+$cuerpo .= '                </div>';
+$cuerpo .= '            </div>';
+$cuerpo .= '        </td>';
+$cuerpo .= '    </tr>';
+$cuerpo .= '</table>';
 
 //para el envio en formato HTML 
 $headers = "MIME-Version: 1.0\r\n"; 
